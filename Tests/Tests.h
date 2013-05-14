3a7e7e734bcf5acb2ed9891580559ae949a165a6
Jon Crooke <joncrooke@gmail.com>
2013-05-14 12:46:10 +0200

    Test bundle

---
+++ Tests/Tests.h
@@ -0,0 +1,13 @@
+//
+//  Tests.h
+//  Tests
+//
+//  Created by Jonathan Crooke on 14/05/2013.
+//  Copyright (c) 2013 Jon Crooke. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase
+
+@end
